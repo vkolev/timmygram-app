c8e8d388c85ac97ac4c48ad632198a85676f3b72
Vladimir Kolev <vladimir.r.kolev@gmail.com>
2026-04-28 12:02:58 +0200

    Initial Commit

---
+++ TimmyGramApp/TimmyGramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimmyGramAppApp.swift
+//  TimmyGramApp
+//
+//  Created by Vladimir Kolev on 28.04.26.
+//
+
+import SwiftUI
+
+@main
+struct TimmyGramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
